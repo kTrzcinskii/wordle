a23b3728678679726e5fd952babd7abf2575ebfb
kTrzcinskii <kt.trzcinski@gmail.com>
2023-02-13 18:52:02 +0100

    Worlde construcotr (loading data from file)

--- src/Wordle.cpp
+++ src/Wordle.cpp
@@ -1,17 +1,38 @@
 #include "Wordle.h"
 #include "TerminalManager.h"
 #include <iostream>
+#include <fstream>
 
 namespace wordle {
 	terminal_manager::TerminalManager tm;
 
+	Wordle::Wordle()
+	{
+		tm.change_color(tm.Cyan);
+		std::cout << "Loading data...\n";
+		tm.sleep_for(300);
+		std::ifstream file("./res/words.txt");
+		std::string temp;
+		while (std::getline(file, temp))
+		{
+			all_words_.push_back(temp);
+			all_words_number_++;
+		}
+		file.close();
+		tm.change_color(tm.Green);
+		std::cout << "Data loaded successfully!\n";
+		tm.sleep_for(500);
+		tm.clear_terminal();
+	}
+
 	void Wordle::welcome_user()
 	{
+		tm.change_color(tm.White);
 		std::cout << "\nWelcome to Worlde - terminal version!\n\n";
 		tm.change_color(tm.Cyan);
 		std::cout << "Enter your name to continue: ";
 		tm.change_color(tm.Violet);
-		getline(std::cin, user_);
+		std::getline(std::cin, user_);
 		tm.sleep_for(500);
 		tm.clear_terminal();
 		tm.change_color(tm.White);
@@ -22,8 +43,14 @@ namespace wordle {
 		std::cout << "\nLet the game begin!\n";
  	}
 
+	void Wordle::set_new_word()
+	{
+		std::cout << "ALL WORDS: " << all_words_number_;
+	}
+
 	void Wordle::start()
 	{
 		welcome_user();
+		set_new_word();
 	}
 }
--- src/Wordle.h
+++ src/Wordle.h
@@ -1,17 +1,21 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace wordle {
 	class Wordle {
 	public:
+		Wordle();
 		void start();
 		void stop();
 		void reset();
 	private:
-		std::string user_;
-		std::string current_word_;
-		unsigned int current_round_;
+		std::string user_{};
+		std::string current_word_{};
+		unsigned int current_round_{};
+		std::vector<std::string> all_words_;
+		unsigned int all_words_number_{};
 		void set_new_word();
 		void welcome_user();
 		void game_loop();
